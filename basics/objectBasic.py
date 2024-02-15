7e9e40eb57e277f91223af8ef3edecfa05149766
zoyron <sgrarora3@gmail.com>
2024-02-15 18:07:35 +0530

    made a objects basics file in python to compare the time and cpu resources of it with a similar code in C++

---
+++ basics/objectBasic.py
@@ -0,0 +1,16 @@
+class Test:
+    def __init__(self):
+        self.age = 100
+        self.car = 8000
+
+    def show_age(self):
+        print("the age is ", self.age)
+
+    def show_car(self):
+        print("the car plate number is ", self.car)
+
+
+
+ob1 = Test()
+ob1.show_age()
+ob1.show_car()
