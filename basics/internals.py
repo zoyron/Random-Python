c5a8bbeea1762da3272f0aa67692d7f4440448da
zoyron <sgrarora3@gmail.com>
2024-02-27 13:06:59 +0530

    added 1 method of equating lists

--- basics/internals.py
+++ basics/internals.py
@@ -1,4 +1,13 @@
 list1 = [1,2,3]
 list2 = list1
+print("here we have list2 = list1, so \"list1 is list2\" would evaluate to True")
+print(list1 is list2)
 print(list1)
 print(list2)
+# in the above example a list with 1,2 and 3 in values is created in the memory then it is reference to the variable list1, after that we referenced list1 to list2, hence giving it the same memory reference as we have in list1, but instead of that had we done list2 = [1,2,3] then although the values in the lists would have been same, but the memory reference would be different. Those 2 lists would have same values but those would be entirely different lists since their memory location would be totally different
+
+list1 = [1,2,3]
+list2 = [1,2,3]
+# in this particular example both the lists have same values but we need to remember that these are two different lists at two different locations in the memory, unlike the 1st example where we made just 1 list and pointed 2 different variables  to it
+
+
