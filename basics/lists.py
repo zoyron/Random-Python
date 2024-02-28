3d9a249e865f27461e4b53e9c04a18e5f8fb699f
zoyron <sgrarora3@gmail.com>
2024-02-29 04:08:48 +0530

    list methods

--- basics/lists.py
+++ basics/lists.py
@@ -3,3 +3,11 @@ print(coffee_varities)
 print(coffee_varities[1:3])
 print(coffee_varities[:2])
 print(coffee_varities[2:])
+
+# lists are mutable
+# it doesn't create a new list when modifying an index, it changes that one only
+for coffee in coffee_varities:
+    print(coffee)
+
+for coffee in coffee_varities:
+    print(coffee, end=" ")
