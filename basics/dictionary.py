ce4ba9177a91c770e95891010046a04bd88a5663
zoyron <sgrarora3@gmail.com>
2024-02-29 04:33:24 +0530

    key value pairs in dict

--- basics/dictionary.py
+++ basics/dictionary.py
@@ -1,1 +1,11 @@
 # dictionary in python
+# dictionaries are also mutable
+coffee_types = {"brew":"cold", "espresso":"hot", "filter":"love"}
+
+for coffee in coffee_types:
+    print(coffee)
+#this would print all the keys
+
+for coffee in coffee_types:
+    print(coffee, coffee_types[coffee])
+#this code would print the key value pairs
