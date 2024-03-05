882cd2a74c9d1d5c0319b825d6fd8c66e1cd6caa
zoyron <sgrarora3@gmail.com>
2024-03-05 13:41:23 +0530

    solved the weather problem

--- basics/conditionalProb.py
+++ basics/conditionalProb.py
@@ -1,14 +1,27 @@
-day = input("give me a day: ")
-age = int(input("give me an age: "))
-ticket_price = 0
-#if age < 18:
-#    ticket_price = 8
-#else:
-#    ticket_price = 12
-# the other way to write the price is as follows
-ticket_price = 12 if age >= 18 else 8
+#day = input("give me a day: ")
+#age = int(input("give me an age: "))
+#ticket_price = 0
+##if age < 18:
+##    ticket_price = 8
+##else:
+##    ticket_price = 12
+## the other way to write the price is as follows
+#ticket_price = 12 if age >= 18 else 8
+#
+#if day == "wednesday":
+#    ticket_price -= 2
+#
+#print(ticket_price)
 
-if day == "wednesday":
-    ticket_price -= 2
+# weather problem
 
-print(ticket_price)
+weather = input("give me the weather condition:")
+
+if weather == "sunny":
+    print("go for a walk.")
+elif weather == "rainy":
+    print("read a book.")
+elif weather == "snowy":
+    print("build a snowman.")
+else:
+    print("give a valid input ")
