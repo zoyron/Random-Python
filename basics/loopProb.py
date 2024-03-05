01fbda7c5c6ee41aed76f646335acee95407438d
zoyron <sgrarora3@gmail.com>
2024-03-05 20:17:44 +0530

    solved the even sum prob

--- basics/loopProb.py
+++ basics/loopProb.py
@@ -1,8 +1,16 @@
 # given a list of numbers, count how many are positive
-numbers = [int(x) for x in input("enter numbers separated by a space: ").split()]
-positive_number_count = 0
+#numbers = [int(x) for x in input("enter numbers separated by a space: ").split()]
+#positive_number_count = 0
+#
+#for num in numbers:
+#    if num >= 0:
+#        positive_number_count+=1
+#print(positive_number_count)
 
-for num in numbers:
-    if num >= 0:
-        positive_number_count+=1
-print(positive_number_count)
+# sum of even numbers
+n = int(input("enter a number: "))
+sum_even = 0
+for i in range(1, n+1):
+    if i%2  == 0:
+        sum_even += i
+print(sum_even)
